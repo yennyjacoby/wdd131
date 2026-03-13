ffd906c301d94b11ad9d484d6005858849b53b31
yennyjacoby <yekaromeroro@gmail.com>
2026-03-13 12:09:58 -0600

    js updated

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,37 +1,27 @@
 const input = document.querySelector('#favchap');
-const button= document.querySelector('button')
-const list = document.querySelector('#list'); //fill in the blank to reference the unordered list element. 
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
+button.addEventListener('click', function () {
 
+    if (input.value.trim() !== '') {
 
-
-li.textContent = input.value;
-deleteButton.textContent= '❌';
-li.append(deleteButton);
-list.append(li);
-
-
-
-//click event listener for the Add Chapter button with addEventListener.
-button.addEventListener('click', function(){
-
-
-    if (input.value.trim() !== '') { 
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
-        
+
         li.textContent = input.value;
-        deleteButton.textContent= '❌';
+        deleteButton.textContent = '❌';
 
         li.append(deleteButton);
         list.append(li);
 
-        deleteButton.addEventListener('click', function (){
+        deleteButton.addEventListener('click', function () {
             list.removeChild(li);
             input.focus();
         });
 
-        input.value='';
+        input.value = '';
         input.focus();
-    }    
+    }
+
 });
