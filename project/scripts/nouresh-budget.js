f9b9987335bf93b9f4f123fc06d40b3ef224ef82
yennyjacoby <yekaromeroro@gmail.com>
2026-04-09 22:09:42 -0600

    updated

--- project/scripts/nouresh-budget.js
+++ project/scripts/nouresh-budget.js
@@ -403,3 +403,10 @@ if (randomBtn && randomMealContainer) {
     `;
   });
 }
+
+let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+document.querySelector("#reviewCounter").textContent = reviewCount;
